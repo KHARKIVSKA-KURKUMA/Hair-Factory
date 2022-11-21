aae1376c8089c429027ad6f4aaf74590923e554b
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2022-11-21 19:24:21 +0200

    add sign/ service

--- js/slider.js
+++ js/slider.js
@@ -21,3 +21,19 @@ $owl.owlCarousel({
     // 1600: { items: 5 },
   },
 });
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
