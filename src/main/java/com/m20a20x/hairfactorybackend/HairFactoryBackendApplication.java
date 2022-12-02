11a8e638bd318999b00ba9d1d8e65bde233c588f
Maksym Shapovalov <maks0681912507@gmail.com>
2022-12-02 15:12:35 +0200

    Initial commit

---
+++ src/main/java/com/m20a20x/hairfactorybackend/HairFactoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.m20a20x.hairfactorybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairFactoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairFactoryBackendApplication.class, args);
+	}
+
+}
