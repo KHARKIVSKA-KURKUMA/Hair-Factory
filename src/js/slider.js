d643b6eb66b4567ed404878cd4ae7bbe5b095eba
Vasiuk Kateryna <catherine.vasyuk11@gmail.com>
2023-04-09 01:55:11 +0300

    update site

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,45 +1,7 @@
-let $owl = $('.carousel-2');
-$owl.children().each(function (index) {
-  $(this).attr('data-position', index);
-});
-$(document).on('click', '.owl-item>div', function () {
-  let $speed = 300;
-  $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
-});
-$owl.owlCarousel({
-  loop: true,
-  center: true,
-  stagePadding: 70,
-  nav: true,
-  dots: false,
-  navText: ['&lsaquo;', '&rsaquo;'],
-  responsive: {
-    0: { items: 1 },
-    800: { items: 2 },
-    1200: { items: 3 },
-    // 1200: { items: 4 },
-    // 1600: { items: 5 },
-  },
-});
-// $('.owl-carousel').owlCarousel({
-//   loop: true,
-//   margin: 10,
-//   nav: true,
-//   responsive: {
-//     0: {
-//       items: 1,
-//     },
-//     600: {
-//       items: 3,
-//     },
-//     1000: {
-//       items: 5,
-//     },
-//   },
-// });
 $('.owl-carousel').owlCarousel({
   margin: 20,
+  nav: true,
   loop: true,
   autoWidth: true,
-  items: 4,
+  items: 3,
 });
