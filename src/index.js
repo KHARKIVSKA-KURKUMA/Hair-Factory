aa38bbb102de18fa1fd886ca277cde838c8768d9
Vasiuk Kateryna <catherine.vasyuk11@gmail.com>
2023-04-08 21:42:52 +0300

    Remove js file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-const CONSULT_URL = 'http://localhost:8080/enroll';
-const CONSULT_ID = 'consultation-form';
+// const CONSULT_URL = 'http://localhost:8080/enroll';
+// const CONSULT_ID = 'consultation-form';
 
-window.addEventListener('load', async _event => {
-  const consultForm = document.getElementById(CONSULT_ID);
+// window.addEventListener('load', async _event => {
+//   const consultForm = document.getElementById(CONSULT_ID);
 
-  consultForm.addEventListener('submit', async event => {
-    event.preventDefault();
+//   consultForm.addEventListener('submit', async event => {
+//     event.preventDefault();
 
-    const response = await fetch(CONSULT_URL, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json;charset=UTF-8',
-      },
-      body: JSON.stringify(Object.fromEntries(new FormData(consultForm))),
-    });
+//     const response = await fetch(CONSULT_URL, {
+//       method: 'POST',
+//       headers: {
+//         'Content-type': 'application/json;charset=UTF-8',
+//       },
+//       body: JSON.stringify(Object.fromEntries(new FormData(consultForm))),
+//     });
 
-    if (!response.ok) {
-      return;
-    }
+//     if (!response.ok) {
+//       return;
+//     }
 
-    const result = await response.text();
+//     const result = await response.text();
 
-    alert(result);
-  });
-});
+//     alert(result);
+//   });
+// });
